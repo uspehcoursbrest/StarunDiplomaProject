cd54615c0d25bd55a6313dfc2524feda86f50c3f
veronika <tapochki22@gmail.com>
2018-11-17 02:55:24 +0300

    сделаны адаптивы под устройства, исправлены ошибки

---
+++ js/script.js
@@ -0,0 +1,25 @@
+window.onload = function () {
+    function clock() {
+        var date = new Date(),
+            day = date.getDate(),
+            month = date.getMonth(),
+            monthArr = ["Янв", "Фев", "Мар", "Апр", "Май", "Июн", "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек"],
+            year = date.getFullYear(),
+            hour = date.getHours(),
+            min = date.getMinutes();
+
+        if (day < 10) day = "0" + day;
+        if (hour < 10) hour = "0" + hour;
+        if (min < 10) min = "0" + min;
+
+        document.getElementById("date").innerHTML = day + " " + monthArr[month];
+        document.getElementById("year").innerHTML = year;
+        document.getElementById("time").innerHTML = hour + ":" + min;
+    }
+    var timer;
+    function clockStart() {
+        timer = setInterval(clock, 60000);
+        clock();
+    }
+    clockStart();
+};
