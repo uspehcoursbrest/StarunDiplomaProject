58c05e5c9ba20c1ede9b9f61e2345afa547f433f
veronika <tapochki22@gmail.com>
2018-11-20 23:11:58 +0300

    добавлена кнопка наверх

--- js/script.js
+++ js/script.js
@@ -22,4 +22,21 @@ window.onload = function () {
         clock();
     }
     clockStart();
+
+    var btnUp = $(".up");
+    btnUp.hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            btnUp.fadeIn();
+        } else {
+            btnUp.fadeOut();
+        }
+    });
+    btnUp.click(function () {
+        $("body,html").animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    })
 };
+
